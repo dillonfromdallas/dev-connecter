dbb33ea55822919bf065375a04a7acc9752b184c
dillonreyna <thedillonreyna@gmail.com>
2018-09-25 16:22:46 -0500

    User model finished

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
   mongoURI:
-    "mongodb://devconnect:devconnect1@ds123658.mlab.com:23658/dev_connect"
+    "mongodb://devconnect:devconnect1@ds123658.mlab.com:23658/dev_connect",
+  secret: "secret"
 };
