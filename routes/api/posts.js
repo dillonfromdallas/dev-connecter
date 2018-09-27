41703e9c91c9ec2928b9ddfdb589723a9b2f2083
dillonreyna <thedillonreyna@gmail.com>
2018-09-26 23:32:56 -0500

    Get personal posts and delete posts.

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -23,7 +23,6 @@ router.post(
     if (!isValid) {
       return res.status(400).json(errors);
     }
-    console.log(1);
     const newPost = new Post({
       text: req.body.text,
       name: req.body.name,
@@ -45,6 +44,21 @@ router.get("/", (req, res) => {
     .catch(err => res.status(404).json({ noPosts: "No posts found!" }));
 });
 
+// @route   GET api/posts/mine
+// @desc    Show all posts you've made
+// @access  Private
+router.get(
+  "/mine",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Post.find({ user: req.user })
+      .then(posts => {
+        res.json(posts);
+      })
+      .catch(err => res.status(404).json(err));
+  }
+);
+
 // @route   GET api/posts/:post_id
 // @desc    Get an individual post
 // @access  Private
@@ -53,5 +67,53 @@ router.get("/:post_id", (req, res) => {
     .then(post => res.json(post))
     .catch(err => res.status(404).json({ noPost: "No post found!" }));
 });
+
+// @route   POST api/posts/:post_id
+// @desc    Update a post
+// @access  Private
+router.post(
+  "/:post_id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validatePostInput(req.body);
+    if (!isValid) {
+      return res.status(400).json(errors);
+    }
+
+    Post.findById(req.params.post_id).then(post => {
+      if (!post.user === req.user) {
+        errors.perm = "You cannot edit someone else's post.";
+        return res.status(400).json(errors);
+      }
+      post.text = req.body.text;
+      post.save().then(res.json(post));
+    });
+  }
+);
+
+// @route   DELETE api/posts/:post_id
+// @desc    Delete a post
+// @access  Private
+router.delete(
+  "/:post_id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Post.findById(req.params.post_id)
+      .then(post => {
+        if (post.user === req.user) {
+          res.json({ success: true });
+        } else {
+          res
+            .status(404)
+            .json({ identityTheft: "You can't delete someone else's posts." });
+        }
+      })
+      .catch(err =>
+        res.status(404).json({
+          postNotFound: "Could not find the post. Maybe it's already deleted!"
+        })
+      );
+  }
+);
 
 module.exports = router;
