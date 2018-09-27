225963ba69defe88efb959ab1b9e75a0f0af9a47
Dillon Reyna <thedillonreyna@gmail.com>
2018-09-26 20:10:15 -0500

    Post GET routes added

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -35,4 +35,23 @@ router.post(
   }
 );
 
+// @route   GET api/posts
+// @desc    Get all posts
+// @access  Public
+router.get("/", (req, res) => {
+  Post.find()
+    .sort({ date: -1 })
+    .then(posts => res.json(posts))
+    .catch(err => res.status(404).json({ noPosts: "No posts found!" }));
+});
+
+// @route   GET api/posts/:post_id
+// @desc    Get an individual post
+// @access  Private
+router.get("/:post_id", (req, res) => {
+  Post.findById(req.params.post_id)
+    .then(post => res.json(post))
+    .catch(err => res.status(404).json({ noPost: "No post found!" }));
+});
+
 module.exports = router;
