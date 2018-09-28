3663de82e2c389382eb72eba5695cc4a91ba1b5b
dillonreyna <thedillonreyna@gmail.com>
2018-09-27 22:05:59 -0500

    Add/delete comment added

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const passport = require("passport");
+const Validator = require("validator");
 
 const router = express.Router();
 
@@ -51,7 +52,7 @@ router.get(
   "/mine",
   passport.authenticate("jwt", { session: false }),
   (req, res) => {
-    Post.find({ user: req.user })
+    Post.find({ user: req.user.id })
       .then(posts => {
         res.json(posts);
       })
@@ -81,9 +82,9 @@ router.post(
     }
 
     Post.findById(req.params.postId).then(post => {
-      if (!post.user === req.user) {
+      if (!post.user === req.user.id) {
         errors.perm = "You cannot edit someone else's post.";
-        return res.status(400).json(errors);
+        return res.status(401).json(errors);
       }
       post.text = req.body.text;
       post.save().then(res.json(post));
@@ -100,19 +101,14 @@ router.delete(
   (req, res) => {
     Post.findById(req.params.postId)
       .then(post => {
-        if (post.user === req.user) {
-          res.json({ success: true });
-        } else {
-          res
-            .status(404)
-            .json({ identityTheft: "You can't delete someone else's posts." });
+        if (post.user.toString() !== req.user.id) {
+          return res
+            .status(401)
+            .json({ identityTheft: `You can't delete another user's posts!` });
         }
+        post.remove().then(() => res.json({ success: true }));
       })
-      .catch(err =>
-        res.status(404).json({
-          postNotFound: "Could not find the post. Maybe it's already deleted!"
-        })
-      );
+      .catch(err => res.status(400).json(err));
   }
 );
 
@@ -143,6 +139,82 @@ router.post(
             res.status(200).json(post);
           })
           .catch(err => res.status(404).json(err));
+      })
+      .catch(err => res.status(400).json(err));
+  }
+);
+
+// @route   POST api/posts/comment/:postId
+// @desc    Add a comment to a post
+// @access  Private
+router.post(
+  "/comment/:postId",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validatePostInput(req.body);
+    if (!isValid) {
+      return res.status(400).json(errors);
+    }
+
+    Post.findById(req.params.postId).then(post => {
+      if (!post) {
+        res.status(404).json({ postNotFound: "No post found." });
+      }
+
+      const newComment = {
+        text: req.body.text,
+        name: req.body.name,
+        avatar: req.body.avatar,
+        user: req.user.id
+      };
+
+      post.comments.unshift(newComment);
+      post
+        .save()
+        .then(res.json(post))
+        .catch(err => res.statur(404).json(err));
+    });
+  }
+);
+
+// @route   DELETE api/posts/comment/:postId/:commentId
+// @desc    Delete a comment from a post
+// @access  Private
+router.delete(
+  "/comment/:postId/:commentId",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    // Find the parent post
+
+    Post.findById(req.params.postId)
+      .then(post => {
+        // Find the comment
+
+        const commentToDelete = post.comments.find(
+          comment => comment._id == req.params.commentId
+        );
+        if (!commentToDelete) {
+          return res
+            .status(404)
+            .json({ commentNotFound: `Could not find the requested comment.` });
+        }
+
+        // Check that user is either comment owner, or post OP.
+
+        if (!post.user == req.user.id) {
+          if (!commentToDelete.user == req.user.id) {
+            return res.status(401).json({
+              identityTheft: `You can't delete another user's comments.`
+            });
+          }
+        }
+
+        // Delete post using MongoDB $pull operator.
+
+        post
+          .updateOne({ $pull: { comments: { _id: req.params.commentId } } })
+          .then(() => res.json({ success: true }))
+          .catch(err => res.status(400).json(err));
       })
       .catch(err => res.status(404).json(err));
   }
