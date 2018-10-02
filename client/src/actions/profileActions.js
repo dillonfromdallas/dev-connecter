7f071ec8c43a98db56816a4866ef3c0f8a385743
dillonreyna <thedillonreyna@gmail.com>
2018-10-02 14:48:48 -0500

    Base Profile display component

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -5,7 +5,8 @@ import {
   GET_PROFILE,
   GET_ERRORS,
   PROFILE_LOADING,
-  CLEAR_CURRENT_PROFILE
+  CLEAR_CURRENT_PROFILE,
+  GET_PROFILES
 } from "./types";
 
 // Get current profile
@@ -24,6 +25,25 @@ export const getCurrentProfile = () => dispatch => {
         // Returns empty profile if user has not created one.
         type: GET_PROFILE,
         payload: {}
+      })
+    );
+};
+
+// Get all profiles
+export const getProfiles = () => dispatch => {
+  dispatch(setProfileLoading());
+  axios
+    .get("/api/profile/all")
+    .then(res =>
+      dispatch({
+        type: GET_PROFILES,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_PROFILES,
+        payload: null
       })
     );
 };
