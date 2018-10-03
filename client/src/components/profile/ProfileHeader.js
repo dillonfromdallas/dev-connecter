373bc06adfc8ca8c85f390854162c330aa74f918
dillonreyna <thedillonreyna@gmail.com>
2018-10-02 19:58:58 -0500

    Completed profile header

--- client/src/components/profile/ProfileHeader.js
+++ client/src/components/profile/ProfileHeader.js
@@ -1,10 +1,90 @@
 import React, { Component } from "react";
 
+import isEmpty from "../../validation/isEmpty";
+
 class ProfileHeader extends Component {
   render() {
+    const { profile } = this.props;
     return (
-      <div>
-        <h1>profile header</h1>
+      <div className="row">
+        <div className="col-md-12">
+          <div className="card card-body bg-info text-white mb-3">
+            <div className="row">
+              <div className="col-4 col-md-3 m-auto">
+                <img
+                  className="rounded-circle"
+                  src={profile.user.avatar}
+                  alt={profile.user.name}
+                />
+              </div>
+            </div>
+            <div className="text-center">
+              <h1 className="display-4 text-center">{profile.user.name}</h1>
+              <p className="lead text-center">
+                {profile.status}
+                {isEmpty(profile.company) ? null : (
+                  <span> at {profile.company}</span>
+                )}
+              </p>
+              <p>
+                {isEmpty(profile.location) ? null : (
+                  <span>{profile.location}</span>
+                )}
+              </p>
+              <p>
+                {isEmpty(profile.website) ? null : (
+                  <a
+                    href={profile.website}
+                    className="text-white p-2"
+                    target="_blank"
+                  >
+                    <i className="fas fa-globe fa-2x" />
+                  </a>
+                )}
+
+                {isEmpty(profile.social && profile.social.facebook) ? null : (
+                  <a
+                    href={profile.social.facebook}
+                    className="text-white p-2"
+                    target="_blank"
+                  >
+                    <i className="fab fa-facebook fa-2x" />
+                  </a>
+                )}
+
+                {isEmpty(profile.social && profile.social.twitter) ? null : (
+                  <a
+                    href={profile.social.twitter}
+                    className="text-white p-2"
+                    target="_blank"
+                  >
+                    <i className="fab fa-twitter fa-2x" />
+                  </a>
+                )}
+
+                {isEmpty(profile.social && profile.social.linkein) ? null : (
+                  <a
+                    href={profile.social.linkedin}
+                    className="text-white p-2"
+                    target="_blank"
+                  >
+                    <i className="fab fa-linkedin fa-2x" />
+                  </a>
+                )}
+
+                {isEmpty(profile.social && profile.social.youtube) ? null : (
+                  <a
+                    href={profile.social.youtube}
+                    className="text-white p-2"
+                    target="_blank"
+                  >
+                    <i className="fab fa-youtube fa-2x" />
+                  </a>
+                )}
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
