c0c6070cb67a67c4ed6cd184b21a51bbdf58ec8a
dillonreyna <thedillonreyna@gmail.com>
2018-10-04 03:52:41 -0500

    All done. Prepared for deploy.

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
-module.exports = {
-  mongoURI:
-    "mongodb://devconnect:devconnect1@ds123658.mlab.com:23658/dev_connect",
-  secret: "secret"
-};
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./keys_prod");
+} else {
+  module.exports = require("./keys_dev");
+}
