ec87a932cfa5a36d943c94b3fdfed4cb7b3f8112
Dillon Reyna <thedillonreyna@gmail.com>
2019-02-21 09:33:44 -0600

    Set email to lowercase

--- routes/api/users.js
+++ routes/api/users.js
@@ -44,7 +44,7 @@ router.post("/register", (req, res) => {
 
         const newUser = new User({
           name: req.body.name,
-          email: req.body.email,
+          email: req.body.email.toLowerCase(),
           avatar,
           password: req.body.password
         });
@@ -100,7 +100,7 @@ router.post("/login", (req, res) => {
         };
 
         // Sign token
-        jwt.sign(payload, keys.secret, { expiresIn: '7d' }, (err, token) => {
+        jwt.sign(payload, keys.secret, { expiresIn: "7d" }, (err, token) => {
           res.json({
             success: true,
             token: `Bearer ${token}`
